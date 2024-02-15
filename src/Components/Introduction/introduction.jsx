5c8f71cfdc94e78bf575fe39960c2f0526be51a8
utkbella <114491739+utkbella@users.noreply.github.com>
2024-02-16 05:01:05 +0530

    Added introduction

--- src/Components/Introduction/introduction.jsx
+++ src/Components/Introduction/introduction.jsx
@@ -2,9 +2,13 @@ import React from "react";
 import './introduction.css';
 const Introduction = () => {
     return (
-        <div>
-            <div>Powerful In-Depth Analytics for Instagram</div>
-            <div> for Instagram</div>
+        <div className="introduction">
+            <div className="caption">Powerful In-Depth Analytics </div>
+            <div className="caption"> for Instagram</div>
+            <div className="c-details">inhub offers powerful in-depth analytics and actionable insights for brands, influencers and agencies. Trusted by millions since 2012. Powered by the official Instagram API.</div>
+            <div className="searchbox">
+                
+            </div>
         </div>
     )
 }
