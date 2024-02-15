71c0ebce6640d007e7600fe7e57482c1e67e3045
utkbella <114491739+utkbella@users.noreply.github.com>
2024-02-16 03:28:48 +0530

    Navbar is set.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
-import Navbar from "./Components/Navbar/navbar"
+import Navbar from "./Components/Navbar/navbar";
+import Introduction from "./Components/Introduction/introduction";
 function App() {
   return (
     <div className="App">
  
    <Navbar/>
+   <Introduction/>
    
     </div>
   );
---
+++ src/Components/Introduction/introduction.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './introduction.css';
+const Introduction = () => {
+    return (
+        <div>
+            <div>Powerful In-Depth Analytics for Instagram</div>
+            <div> for Instagram</div>
+        </div>
+    )
+}
+export default Introduction
+
